5be96bf8da34033857fadcef0c06bf1d55d8ca09
MikeRifRaf <m.oneil@rifrafgames.com>
2019-11-06 09:37:08 -0700

    When playing the Cargo mini game you only play the original puzzle 1.

--- Assets/Scripts/StaticStuff.cs
+++ Assets/Scripts/StaticStuff.cs
@@ -6,12 +6,12 @@ public static class StaticStuff
 {    
     static public void FlowDebug(string s)
     {
-        return;
+       // return;
         Debug.Log(s);
     }
     static public void FlowDebugWarning(string s)
     {
-        return;
+        //return;
         Debug.LogWarning(s);
     }
 }
